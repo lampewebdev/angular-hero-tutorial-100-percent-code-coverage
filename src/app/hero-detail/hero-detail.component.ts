a0fa9e884381d3e1f39e5d90f1f00a1afbabf04b
Lazarski, Michael (OTD) <michael.lazarski@tchibo.de>
2021-01-29 18:00:36 +0100

    hero example done

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,40 @@
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero!: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id){
+      this.heroService.getHero(parseInt(id, 10))
+      .subscribe(hero => this.hero = hero);
+    }
+  }
+  save(): void {
+    if(this.hero){
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
+  goBack(): void {
+    this.location.back();
+  }
+}
